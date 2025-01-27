a4319675bb8f7b99eac7e3d795a961d6c089c3f1
JiMare <zoa@seznam.cz>
2025-01-27 21:46:19 +0100

    fix menu

--- src/components/layout/MenuWrapper.ts
+++ src/components/layout/MenuWrapper.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MenuWrapper = styled.div`
   position: absolute;
-  bottom: 2rem;
+  bottom: 5rem;
   width: 24.375rem;
   padding: var(--spacing-lg);
 `;
